bed24e559c5da8656466e244d8608aa26c535a19
Radium Rocket 32 <radiumrocket32@Radiums-MacBook-Pro-2.local>
2023-07-12 16:25:11 -0300

    challenge04-004: fix custom commands path

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -20,7 +20,7 @@ module.exports = {
   page_objects_path: ['nightwatch/page-objects'],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
-  custom_commands_path: ['nightwatch/custom-commands'],
+  custom_commands_path: ['node_modules/nightwatch/examples/custom-commands/'],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
   custom_assertions_path: '',
